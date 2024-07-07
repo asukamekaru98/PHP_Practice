49543684367458236bb73c44d0f38bcfde09e149
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-07 23:06:58 +0900

    3-2

---
+++ PHP7_MySQL/htdocs/php_note/chap03/3-2/switch_2.php
@@ -0,0 +1,18 @@
+<?php
+$color = "yellow";
+$price = 100;
+
+switch($color){
+    case "green":
+        $price = 120;
+        break;
+        case "red":
+            $price = 140;
+            break;
+
+
+}
+
+echo "{$color}は{$price}円";
+
+?>
