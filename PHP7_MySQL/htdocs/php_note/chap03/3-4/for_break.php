f8cf09e2dd788e587dc1e9af3f07221142b332e3
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-09 09:47:25 +0900

    <pre>はpreformatted text（整形済みテキスト）のこと。 <pre>～</pre>で囲んだ整形済みテキストを表示するためのタグ。 整形済みテキストとはあらかじめマークアップを行う前に整形してあるテキストのことで、ソース中のスペースや改行などをそのまま等幅フォントで表示する。

---
+++ PHP7_MySQL/htdocs/php_note/chap03/3-4/for_break.php
@@ -0,0 +1,16 @@
+<?php
+$list = array(20,-32,50,-5,40);
+
+$count = count($list);
+$sum = 0;
+
+for($i = 0;$i <= $count; $i++){
+    $value = $list[$i];
+    if($value < 0){
+        $sum = "マイナスの値{$value}が含まれていたので中止しました。";
+        break;
+    }else{
+        $sum += $value;
+    }
+}
+echo "合計：$sum";
