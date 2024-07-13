89b650a2c38614e439b6306aa9bbe82a3707240e
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-13 14:10:46 +0900

    5-2

---
+++ PHP7_MySQL/htdocs/php_note/chap05/5-2/format_precision.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10.2582;
+$b = 3.1;
+$c = "Hypertext Preprocessor!";
+
+printf('%.2f',$a);
+echo "<br>";
+printf('%.02f',$b);
+echo "<br>";
+printf('%.5s',$c);
---
+++ PHP7_MySQL/htdocs/php_note/chap05/5-2/number_format2.php
@@ -0,0 +1,4 @@
+<?php
+$num = 235.365;
+$length = number_format($num,2);
+echo $length,"m";
