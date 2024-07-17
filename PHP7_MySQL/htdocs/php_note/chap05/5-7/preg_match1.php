946faad4ce9f35bb1047bcc2076f10205f286986
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-17 09:04:18 +0900

    5-7 途中

---
+++ PHP7_MySQL/htdocs/php_note/chap05/5-7/preg_match1.php
@@ -0,0 +1,8 @@
+<?php
+$result1 = preg_match("/46-49/u", "確か49-46でした");
+$result2 = preg_match("/46-49/u", "確か46-49でした");
+$result3 = preg_match("/46-49u", "46-49");
+
+var_dump($result1);
+var_dump($result2);
+var_dump($result3);
