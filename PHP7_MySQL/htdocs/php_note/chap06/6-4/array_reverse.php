b8e0c5cb6ccdf2723a9628bb2ce2324af5fcb98b
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-20 18:58:30 +0900

    section6-4 ・sort 昇順にソートする ・rsort 降順にソートする ・asort/arsort インデックスのキーがリセットされないソート ・shuffle ランダムに並び替える ・array_reverse 配列の並びを逆にする ・natsort自然順に並べる

---
+++ PHP7_MySQL/htdocs/php_note/chap06/6-4/array_reverse.php
@@ -0,0 +1,4 @@
+<?php
+$nameList = ["田中", "鈴木", "砂糖", "杉山"];
+$result = array_reverse($nameList);
+print_r($result);
---
+++ PHP7_MySQL/htdocs/php_note/chap06/6-4/shuffle.php
@@ -0,0 +1,4 @@
+<?php
+$nameList = ["田中", "鈴木", "砂糖", "杉山"];
+shuffle($nameList);
+print_r($nameList);
