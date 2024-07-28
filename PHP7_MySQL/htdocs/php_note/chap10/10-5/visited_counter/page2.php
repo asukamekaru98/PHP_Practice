17592874bfa991a8d16e2011d22e7c0d22b9cbe5
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-07-28 22:42:26 +0900

    section10-5

---
+++ PHP7_MySQL/htdocs/php_note/chap10/10-5/visited_counter/page2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>page2</title>
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <a href="page1.php">Page1に戻る</a>
+    </div>
+</body>
+
+</html>
