dc02bbe1de130891288871567bd23ca049a12543
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-08-07 09:10:29 +0900

    chapter3-2 コロンで区切ったswitch文は、改行をするとエラーになる。 構文訂正ツールだと勝手に改行するので、注意。

---
+++ PHP8_MySQL/htdocs/php_note/chap03/3-2/switch_3.php
@@ -0,0 +1,15 @@
+<?php
+$color = "green";
+switch ($color) {
+    case "green":
+    case "red":
+        $price = 140;
+        break;
+    case "blue":
+        $price = 160;
+        break;
+    default:
+        $price = 100;
+        break;
+}
+echo "{$color}は{$price}円";
---
+++ PHP8_MySQL/htdocs/php_note/chap03/3-2/switch_endswitch.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>コロンで区切ったswitch</title>
+</head>
+
+<body>
+    <?php
+    $color = "red";
+    ?>
+
+    <?php switch ($color): ?>
+    <?php
+        case "green": ?>
+            緑は120円<br>
+            <?php break; ?>
+
+        <?php
+        case "red": ?>
+            赤は150円<br>
+            <?php break; ?>
+
+        <?php
+        default: ?>
+            20以上は2500円<br>
+            <?php break; ?>
+    <?php endswitch; ?>
+
+</body>
+
+</html>
