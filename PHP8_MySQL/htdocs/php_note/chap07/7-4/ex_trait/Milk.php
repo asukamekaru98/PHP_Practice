043597cea8e2b44cb7186471672c99e8a4056647
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-08-11 22:55:26 +0900

    section7-4

---
+++ PHP8_MySQL/htdocs/php_note/chap07/7-4/ex_trait/Milk.php
@@ -0,0 +1,16 @@
+<?php
+require_once("DateTool.php");
+
+class Milk
+{
+    use DateTool;
+    public String $theDate;
+    public String $limitDate;
+
+    function __construct()
+    {
+        $now = new DateTime();                              //時間取得
+        $this->theDate = $this->ymdString($now);            //時間を文字化
+        $this->limitDate = $this->addYmdString($now, 10);   //時間+10日を文字化
+    }
+}
