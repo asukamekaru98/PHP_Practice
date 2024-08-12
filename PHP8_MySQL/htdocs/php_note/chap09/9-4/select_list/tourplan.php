dfd8a05582c6dc14f41b61d80b84b3dd66bcbb36
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-08-12 18:06:13 +0900

    section9-5

---
+++ PHP8_MySQL/htdocs/php_note/chap09/9-4/select_list/tourplan.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>リストボックス</title>
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <?php
+        require_once("../../lib/util.php");
+        if (!cken($_POST)) {
+            $encoding = mb_internal_encoding();
+            $err = "Encoding Error!" . $encoding;
+            exit($err);
+        }
+        $_POST = es($_POST);
+        ?>
+
+        <?php
+        $error = [];
+        if (isset($_POST['meal'])) {
+            $meals = ["朝食", "夕食"];
+            $diffValue = array_diff($_POST['meal'], $meals);
+
+            if (count($diffValue) == 0) {
+                $mealSelected = $_POST["meal"];
+            } else {
+                $mealSelected = [];
+                $error[] = "「朝食」に入力エラーあり";
+            }
+        } else {
+            $mealSelected = [];
+        }
+
+        if (isset($_POST['tour'])) {
+            $tours = ["カヌー", "MTB", "トレラン"];
+            $diffValue = array_diff($_POST['tour'], $tours);
+
+            if (count($diffValue) == 0) {
+                $tourSelected = $_POST["tour"];
+            } else {
+                $tourSelected = [];
+                $error[] = "「ツアー」に入力エラーあり";
+            }
+        } else {
+            $tourSelected = [];
+        }
+        ?>
+
+        <?php
+        function selected(string $value, array $selectedValues)
+        {
+            $isSelected = in_array($value, $selectedValues);
+
+            if ($isSelected) {
+                echo "selected";
+            }
+        }
+        ?>
+
+        <form method="POST" action="<?php echo es($_SERVER['PHP_SELF']); ?>">
+            <ul>
+                <li><span>食事：</span>
+                    <select name="meal[]" size=2 multiple>
+                        <option value="朝食" <?php selected("朝食", $mealSelected); ?>>朝食</option>
+                        <option value="夕食" <?php selected("夕食", $mealSelected); ?>>夕食</option>
+                    </select>
+                </li>
+                <li><span>ツアー：</span>
+                    <select name="tour[]" size=3[\:^ 0] multiple>
+                        <option value="カヌー" <?php selected("カヌー", $tourSelected); ?>>カヌー</option>
+                        <option value="MTB" <?php selected("MTB", $tourSelected); ?>>MTB</option>
+                        <option value="トレラン" <?php selected("トレラン", $tourSelected); ?>>トレラン</option>
+                    </select>
+                </li>
+                <li><input type="submit" value="送信"></li>
+            </ul>
+        </form>
+
+        <?php
+        $isSelected = count($mealSelected) > 0 && count($tourSelected) > 0;
+        echo "<HR>";
+        if ($isSelected) {
+            echo "お食事：", implode("と", $mealSelected), "<br>";
+            echo "ツアー：", implode("と", $tourSelected), "<br>";
+        } else {
+            echo "選択なし";
+        }
+        ?>
+
+        <?php
+        if (count($error) > 0) {
+            echo "<HR>";
+            echo '<span class = "error">', implode("<br>", $error), '</span>';
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
---
+++ PHP8_MySQL/htdocs/php_note/chap09/9-5/input_range/slider.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>スライダー</title>
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <?php
+        require_once("../../lib/util.php");
+        if (!cken($_POST)) {
+            $encoding = mb_internal_encoding();
+            $err = "Encoding Error!" . $encoding;
+            exit($err);
+        }
+        $_POST = es($_POST);
+        ?>
+
+        <?php
+        $error = [];
+        $min = 1;
+        $max = 5;
+
+        if (isset($_POST["taste"])) {
+            $taste = $_POST["taste"];
+
+            $isTaste = ctype_digit($taste) && ($taste >= $min) && ($taste <= $max);
+            if (!$isTaste) {
+                $error[] = "甘味の値に入力エラーあり";
+                $taste = $min;
+            }
+        } else {
+            $taste = round(($min + $max) / 2);
+            $isTaste = true;
+        }
+        ?>
+
+        <form method="POST" action="<?php echo es($_SERVER['PHP_SELF']); ?>">
+            <ul>
+                <li><span>甘味：</span>
+                    <input type="range" name="taste" min="1" max="5" step="1" value="<?php echo es($taste); ?>">
+                </li>
+                <li><input type="submit" value="送信する"></li>
+            </ul>
+        </form>
+
+        <?php
+        if ($isTaste) {
+            $tasteList = ["甘い", "少し甘い", "普通", "少し苦い", "苦い"];
+            echo "<HR>";
+            echo "甘味は「{$taste}.{$tasteList[$taste - 1]}」です。";
+        }
+        ?>
+
+        <?php
+        if (count($error) > 0) {
+            echo "<HR>";
+            echo '<span class = "error">', implode("<br>", $error), '</span>';
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
