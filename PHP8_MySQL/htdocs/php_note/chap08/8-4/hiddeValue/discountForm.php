56d3e9abad241022927c8268ad44904eea060bf4
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-08-12 17:07:05 +0900

    section8-4

---
+++ PHP8_MySQL/htdocs/php_note/chap08/8-4/hiddeValue/discountForm.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>割引購入ページ</title>
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <?php
+
+        require_once("../../lib/util.php");
+        if (!cken($_POST)) {
+            $encoding = mb_internal_encoding();
+            $err = "Encoding Error!" . $encoding;
+            exit($err);
+        }
+        $_POST = es($_POST);
+        ?>
+
+        <?php
+        if (isset($_POST['kosu'])) {
+            $kosu = $_POST['kosu'];
+        } else {
+            $kosu = "";
+        }
+        ?>
+
+        <?php
+        $discount = 0.8;
+        $off = (1 - $discount) * 100;
+        if ($discount > 0) {
+            echo "<h2>このページでのご購入は{$off}%OFFになります!</h2>";
+        }
+
+        $tanka = 2900;
+        $tanka_fmt = number_format($tanka);
+        ?>
+
+        <form method="POST" action="discount.php">
+            <input type="hidden" name="discount" value="<?php echo $discount; ?>">
+            <input type="hidden" name="tanka" value="<?php echo $tanka; ?>">
+            <ul>
+                <li><label>単価:<?php echo $tanka_fmt; ?>円</label></li>
+                <li><label>個数:<input type="number" name="kosu" value="<?php echo $kosu; ?>"></label></li>
+                <li><input type="submit" value="送信"></li>
+            </ul>
+        </form>
+    </div>
+</body>
+
+</html>
