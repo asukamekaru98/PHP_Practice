65118f0e2f279aa0f26d6527d91a7bd79a6ad42c
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-12-05 16:11:47 +0900

    7-3~7-4

--- chouzetsu/chap7/src/App/FizzBuzzSeqencePrinter.php
+++ chouzetsu/chap7/src/App/FizzBuzzSeqencePrinter.php
@@ -3,22 +3,26 @@
 namespace FizzBuzz\App;
 
 use FizzBuzz\Core2\NumberConverter;
-use FizzBuzz\Spec2\CyclicNumberRule;
-use FizzBuzz\Spec2\PassThroughRule;
-
 
 class FizzBuzzSeqencePrinter
 {
+	public function __construct(
+		protected NumberConverter $fizzbuzz,
+		protected OutputInterface $output
+	) {}
+
 	public function printRange(int $begin, int $end): void
 	{
-		$fizzBuzz = new NumberConverter([
-			new CyclicNumberRule(3, "Fizz"),
-			new CyclicNumberRule(5, "Buzz"),
-			new PassThroughRule()
-		]);
 
 		for ($i = $begin; $i <= $end; $i++) {
-			printf("%d %s\n", $i, $fizzBuzz->convert($i));
+			$text = $this->fizzbuzz->convert($i);
+			$formattedText = sprintf("%d %s\n", $i, $text);
+			$this->output->write($formattedText);
 		}
 	}
+}
+
+interface OutputInterface
+{
+	public function write(string $data): void;
 }
---
+++ chouzetsu/chap7/test/App/FizzBuzzSequencePrinterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FizzBuzz\App;
+
+use FizzBuzz\Core2\NumberConverter;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzSequencePrinterTest extends TestCase
+{
+	public function testPrintNone(): void
+	{
+		$converter = $this->createMock(NumberConverter::class);
+		$converter->expects($this->never())->method('convert');
+
+		$output = $this->createMock(OutputInterface::class);
+		$output->expects($this->never())->method('write');
+
+		//$printer = new FizzBuzzSeqencePrinter($converter, $output);
+		//$printer->printRange(0, -1);
+	}
+
+	public function testPrint1To3(): void
+	{
+		$converter = $this->createMock(NumberConverter::class);
+		$converter->expects($this->exactly(3))->method('convert')->will($this->returnValueMap([1, "1"], [2, "2"], [3, "Fizz"]));
+
+		$output = $this->createMock(OutputInterface::class);
+		$output->expects($this->exactly(3))->method('write')->withConsecutive(["1 1\n"], ["2 2\n"], ["3 Fizz\n"]);
+
+		//$printer = new FizzBuzzSeqencePrinter($converter, $output);
+		//$printer->printRange(1, 3);
+	}
+}
